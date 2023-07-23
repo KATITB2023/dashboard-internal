d27725765f655d06fce5fbabe60d750946ca9a9c
PanjiSri <neiabaraja04@gmail.com>
2023-07-23 23:23:17 +0700

    Update editAttendance

--- src/server/api/routers/Event_and_EditAttendance.ts
+++ src/server/api/routers/Event_and_EditAttendance.ts
@@ -10,12 +10,13 @@ import { z } from 'zod';
 import {
   createTRPCRouter,
   mentorProcedure,
+  publicProcedure,
   protectedProcedure
 } from '~/server/api/trpc';
 import { prisma } from '~/server/db';
 
 export const eventRouter = createTRPCRouter({
-  getEventList: mentorProcedure.query(async ({ ctx }) => {
+  getEventList: publicProcedure.query(async ({ ctx }) => {
     try {
       const attendanceDaysWithEvents = await ctx.prisma.attendanceDay.findMany({
         include: {
@@ -30,53 +31,42 @@ export const eventRouter = createTRPCRouter({
       });
     }
   }),
-
-  editAttendance: mentorProcedure
-    .input(
-      z.object({
-        attendanceId: z.string().uuid(),
-        status: z.nativeEnum(Status),
-        reason: z.string().optional()
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      const { attendanceId, status, reason } = input;
-      try {
-        const existingAttendance = await ctx.prisma.attendanceRecord.findUnique(
-          {
-            where: { id: attendanceId }
-          }
-        );
-
-        if (!existingAttendance) {
-          throw new TRPCError({
-            code: 'BAD_REQUEST',
-            message: 'Attendance tidak ditemukan.'
-          });
-        }
-
-        // Kalau status kehadiran adalah TIDAK_HADIR dan reason ada, maka tambahin reason
-        if (status === Status.TIDAK_HADIR && reason) {
-          await ctx.prisma.attendanceRecord.update({
-            where: { id: attendanceId },
-            data: { status, reason }
-          });
-        } else {
-          // Kalau status selain TIDAK_HADIR, update status aja
-          await ctx.prisma.attendanceRecord.update({
-            where: { id: attendanceId },
-            data: { status }
-          });
-        }
-
-        return {
-          message: 'Edit attendance berhasil'
-        };
-      } catch (error) {
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'Gagal untuk mengedit attendance.'
-        });
-      }
+  editAttendance: publicProcedure
+  .input(
+    z.object({
+      attendanceId: z.string().uuid(),
+      kehadiran: z.nativeEnum(Status),
+      reason: z.string().optional()
     })
+  )
+  .mutation(async ({ ctx, input }) => {
+    const attendance = await ctx.prisma.attendanceRecord.update({
+      where: {
+        id: input.attendanceId
+      },
+      data: {
+        status: input.kehadiran
+      }
+    });
+
+    if (input.kehadiran !== Status.HADIR && !input.reason) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Please provide a reason"
+      });
+    }
+
+    await ctx.prisma.attendanceRecord.update({
+      where: {
+        id: input.attendanceId,
+      },
+      data: {
+        status: input.kehadiran,
+        reason: input.kehadiran !== Status.HADIR ? input.reason : undefined,
+      },
+    });
+    return {
+      message: "Edit attendance successful"
+    };
+  })
 });
