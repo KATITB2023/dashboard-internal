19f5315d0ffc8fc49f0da1f28931d87b7aaaba1a
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-07-28 11:12:43 +0700

    refactor: unneccesary files and envs

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,16 +1,14 @@
-import { storageRouter } from "~/server/api/routers/storage";
-import { assignmentRouter } from "~/server/api/routers/assignment";
-import { createTRPCRouter } from "~/server/api/trpc";
-import { attendanceRouter } from "~/server/api/routers/attendance";
+import { assignmentRouter } from '~/server/api/routers/assignment';
+import { createTRPCRouter } from '~/server/api/trpc';
+import { attendanceRouter } from '~/server/api/routers/attendance';
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  storage: storageRouter,
   assignment: assignmentRouter,
-  attendance: attendanceRouter,
+  attendance: attendanceRouter
 });
 
 // export type definition of API
