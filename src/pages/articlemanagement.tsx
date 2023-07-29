e516ebfe6ac7edacf424d1944277506d832f85cd
Claudia <88203931+clauculus@users.noreply.github.com>
2023-07-30 00:28:39 +0700

    feat: add hook form

--- src/pages/articlemanagement.tsx
+++ src/pages/articlemanagement.tsx
@@ -8,13 +8,37 @@ import {
   Spacer,
   Button
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { type BaseSyntheticEvent, useState } from 'react';
+import { type SubmitHandler, useForm, Controller } from 'react-hook-form';
+
+interface FormValue {
+  headline: string;
+  keyword: string;
+  article: string;
+  imageUrl: string;
+}
 
 export default function ArticleManagement() {
   const [isPreviewMode, setIsPreviewMode] = useState<boolean>(false);
 
+  const { control, register, formState, setValue, getValues, handleSubmit } =
+    useForm<FormValue>({
+      mode: 'onSubmit',
+      defaultValues: {
+        headline: '',
+        keyword: '',
+        article: '',
+        imageUrl: ''
+      }
+    });
+
   const handlePreviewMode: React.MouseEventHandler<HTMLButtonElement> = () =>
     setIsPreviewMode(!isPreviewMode);
+
+  const submitArticle: SubmitHandler<FormValue> = (data: FormValue) => {
+    // TO DO : submit
+    console.log('submit article', data);
+  };
 
   return (
     <Layout title='Article Management'>
@@ -32,54 +56,83 @@ export default function ArticleManagement() {
           <Text>Article CMS</Text>
         </Flex>
 
-        <Flex direction='column'>
-          <Flex alignItems='center'>
-            <Text
-              fontFamily='SomarRounded-Bold'
-              fontSize='42px'
-              marginRight='16px'
-            >
-              Headline:
-            </Text>
-            <Input
+        <form
+          onSubmit={(e: BaseSyntheticEvent) =>
+            void handleSubmit(submitArticle)(e)
+          }
+        >
+          <Flex direction='column'>
+            <Flex alignItems='center'>
+              {isPreviewMode ? (
+                <Text
+                  fontFamily='SomarRounded-Bold'
+                  fontSize='42px'
+                  marginRight='16px'
+                >
+                  {getValues('headline')}
+                </Text>
+              ) : (
+                <>
+                  <Text
+                    fontFamily='SomarRounded-Bold'
+                    fontSize='42px'
+                    marginRight='16px'
+                  >
+                    Headline:
+                  </Text>
+                  <Input
+                    isDisabled={isPreviewMode}
+                    borderColor='gray.300'
+                    borderWidth='2px'
+                    {...register('headline')}
+                  />
+                </>
+              )}
+            </Flex>
+            <Flex marginTop='10px'>
+              <Flex alignItems='center'>
+                <Text
+                  fontFamily='SomarRounded-Regular'
+                  fontSize='28px'
+                  marginRight='16px'
+                >
+                  Keyword:
+                </Text>
+                <Input
+                  isDisabled={isPreviewMode}
+                  borderColor='gray.300'
+                  borderWidth='2px'
+                  {...register('keyword')}
+                />
+              </Flex>
+              <Spacer />
+              {!isPreviewMode && (
+                <Button onClick={handlePreviewMode}>Preview</Button>
+              )}
+            </Flex>
+            <Textarea
               isDisabled={isPreviewMode}
+              placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit...'
+              marginY='8'
+              height='300px'
               borderColor='gray.300'
               borderWidth='2px'
+              {...register('article')}
             />
-          </Flex>
-          <Flex marginTop='10px'>
-            <Flex alignItems='center'>
-              <Text
-                fontFamily='SomarRounded-Regular'
-                fontSize='28px'
-                marginRight='16px'
-              >
-                Keyword:
-              </Text>
-              <Input
-                isDisabled={isPreviewMode}
-                borderColor='gray.300'
-                borderWidth='2px'
-              />
+            <Flex justifyContent='flex-end'>
+              {!isPreviewMode ? (
+                <>
+                  <Button variant={'outline'} marginRight='10px' type='submit'>
+                    Confirm
+                  </Button>
+                  <Button>Cancel</Button>
+                </>
+              ) : (
+                <Button onClick={handlePreviewMode}>Back</Button>
+              )}
             </Flex>
-            <Spacer />
-            <Button onClick={handlePreviewMode}>Preview</Button>
           </Flex>
-          <Textarea
-            isDisabled={isPreviewMode}
-            placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit...'
-            marginY='8'
-            height='300px'
-            borderColor='gray.300'
-            borderWidth='2px'
-          />
-          <Flex justifyContent='flex-end'>
-            <Button variant={'outline'} marginRight='10px'>
-              Confirm
-            </Button>
-            <Button>Cancel</Button>
-          </Flex>
-        </Flex>
+        </form>
       </Box>
     </Layout>
   );
