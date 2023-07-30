579d30e6ec6a1575c8862effda3841692b218f45
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-07-31 00:55:21 +0700

    feat: add mentor and admin get group data

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import {
   adminProcedure,
@@ -14,10 +15,95 @@ export const groupRouter = createTRPCRouter({
     .input(z.object({ groupId: z.string().uuid() }))
     .query(async ({ ctx, input }) => {
       // TODO: isi logic disini
+      const group = await ctx.prisma.groupRelation.findMany({
+        where: {
+          groupId: input.groupId
+        },
+        include: {
+          user: {
+            select: {
+              id: true,
+              nim: true,
+              profile: {
+                select: {
+                  name: true,
+                  faculty: true,
+                  campus: true
+                }
+              },
+              submission: {
+                select: {
+                  id: true,
+                  filePath: true
+                }
+              },
+              attendance: {
+                select: {
+                  id: true,
+                  date: true,
+                  status: true
+                }
+              }
+            }
+          }
+        }
+      });
+
+      return group;
     }),
 
   mentorGetGroupData: mentorProcedure.query(async ({ ctx }) => {
     // TODO: isi logic disini
+    const mentorId = ctx.session.user.id;
+
+    const groupRelation = await ctx.prisma.groupRelation.findFirst({
+      where: {
+        userId: mentorId
+      }
+    });
+
+    if (!groupRelation) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'Group not found'
+      });
+    }
+
+    const group = await ctx.prisma.groupRelation.findMany({
+      where: {
+        groupId: groupRelation.groupId
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            nim: true,
+            profile: {
+              select: {
+                name: true,
+                faculty: true,
+                campus: true
+              }
+            },
+            submission: {
+              select: {
+                id: true,
+                filePath: true
+              }
+            },
+            attendance: {
+              select: {
+                id: true,
+                date: true,
+                status: true
+              }
+            }
+          }
+        }
+      }
+    });
+
+    return group;
   }),
 
   mentorEditGroupName: mentorProcedure
