bf6c690b16c1f7e87cc5c65fb5368dfd84118bec
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-08-05 13:25:14 +0700

    feat: csv data + fix bulk db entry

--- src/server/api/routers/assignment.ts
+++ src/server/api/routers/assignment.ts
@@ -237,6 +237,9 @@ export const assignmentRouter = createTRPCRouter({
             }
           }),
           ctx.prisma.user.findMany({
+            where: {
+              role: 'STUDENT'
+            },
             select: {
               id: true
             }
---
+++ src/server/api/routers/csv.ts
@@ -0,0 +1,73 @@
+import { z } from 'zod';
+import { adminProcedure, createTRPCRouter } from '~/server/api/trpc';
+
+export const csvRouter = createTRPCRouter({
+  adminGetCSVAssignment: adminProcedure
+    .input(z.object({ assignmentId: z.string().uuid() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.assignment.findMany({
+        where: {
+          id: input.assignmentId
+        },
+        include: {
+          submission: {
+            select: {
+              filePath: true,
+              score: true,
+              createdAt: true,
+              student: {
+                select: {
+                  nim: true,
+                  profile: {
+                    select: {
+                      name: true,
+                      faculty: true,
+                      campus: true
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      });
+    }),
+
+  adminGetCSVAttendance: adminProcedure
+    .input(z.object({ dayId: z.string().uuid() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.attendanceDay.findMany({
+        where: {
+          id: input.dayId
+        },
+        include: {
+          event: {
+            select: {
+              title: true,
+              startTime: true,
+              endTime: true,
+              record: {
+                select: {
+                  date: true,
+                  status: true,
+                  reason: true,
+                  student: {
+                    select: {
+                      nim: true,
+                      profile: {
+                        select: {
+                          name: true,
+                          faculty: true,
+                          campus: true
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      });
+    })
+});
