913fb8fc8cd277eb3aba00ba3799d880647c1182
16721237@mahasiswa.itb.ac.id <16721237@mahasiswa.itb.ac.id>
2023-08-07 23:23:13 +0700

    march 7th > kafka

--- src/components/add-point/index.tsx
+++ src/components/add-point/index.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Flex,
+  Image,
   Input,
   InputGroup,
   Menu,
@@ -28,22 +29,27 @@ import React, { useState } from 'react';
 import { RouterOutputs, api } from '~/utils/api';
 import { Status } from '@prisma/client';
 import { AddPointRow } from './AddPointRow';
+import Layout from '~/layout';
 
 type leaderboardQueryOutput =
   RouterOutputs['leaderboard']['mentorGetLeaderboardData']['data'][0];
 
-export const MentorRecap = () => {
+export const AddPoint = () => {
   const toast = useToast();
 
-  const editRecordMutation = api.attendance.editAttendanceRecord.useMutation();
+  const editLeaderboardScoreMutation =
+    api.leaderboard.mentorUpdateLeaderboardScore.useMutation();
 
   const [page, setPage] = useState<number>(1);
   const [rowPerPage, setRowPerPage] = useState<number>(5);
   const [filterBy, setFilterBy] = useState<string>('');
   const [searchQuery, setSearchQuery] = useState<string>('');
 
+  const groupDataQuery = api.group.mentorGetGroupData.useQuery();
+  const groupData = groupDataQuery.data || [];
+  const groupNumber = groupData[0] ? groupData[0].group.group : -1;
+
   const leaderboardQuery = api.leaderboard.mentorGetLeaderboardData.useQuery({
-    /* dayId: dayId, */
     currentPage: page,
     limitPerPage: rowPerPage,
     filterBy: filterBy,
@@ -57,7 +63,11 @@ export const MentorRecap = () => {
     total: 0
   };
 
-  const maxPage = Math.ceil(leaderboardListMetaData.total / rowPerPage);
+  const maxPage = Math.ceil(
+    leaderboardListMetaData.total
+      ? leaderboardListMetaData.total / rowPerPage
+      : 100
+  );
 
   const {
     isOpen: isEditingRowPerPageOpen,
@@ -112,175 +122,219 @@ export const MentorRecap = () => {
     }
   };
 
-  const editRecord = (
-    record: leaderboardQueryOutput,
-    { newStatus, newDesc }: { newStatus: Status; newDesc: string },
-    successFn: () => void
-  ) => {
-    if (newStatus === record.status) {
-      return;
-    }
-    editRecordMutation
+  const editPoint = (id: string, point: number) => {
+    editLeaderboardScoreMutation
       .mutateAsync({
-        attendanceId: record.id,
-        kehadiran: newStatus,
-        reason: newDesc
+        userId: id,
+        point: point
       })
       .then(() => {
         toast({
-          title: 'Status berhasil diubah',
+          title: 'Berhasil mengubah poin',
           status: 'success',
           duration: 3000,
           isClosable: true
         });
-        successFn();
         leaderboardQuery.refetch();
+      })
+      .catch(() => {
+        toast({
+          title: 'Gagal mengubah poin',
+          status: 'error',
+          duration: 3000,
+          isClosable: true
+        });
       });
   };
 
   return (
-    <Flex flexDir='column'>
-      <Flex justifyContent='space-between'>
-        <Flex alignItems='center' mt='1em'>
-          <>
+    <Layout type='mentor' title='Tambah Poin' fullBg>
+      <Flex flexDir='column'>
+        <Flex
+          justifyContent={{
+            base: 'center',
+            lg: 'space-between'
+          }}
+          flexDir={{
+            base: 'column',
+            lg: 'row'
+          }}
+          w='100%'
+        >
+          <Text
+            color='#340C8F'
+            fontSize='2xl'
+            w='20em'
+            textAlign='left'
+            my='1em'
+            fontWeight='bolder'
+          >
+            Tambah Poin
+          </Text>
+          <Flex
+            bg='black'
+            p='1em'
+            borderRadius='10px'
+            pos='relative'
+            w='min(20em,90%)'
+            alignItems='center'
+            overflow='hidden'
+          >
+            <Text color='white' fontSize='3xl' fontWeight='bold' zIndex='2'>
+              {`Kelompok ${groupNumber === -1 ? 'xx' : groupNumber}`}
+            </Text>
+            <Image
+              src='/images/komet-absen.png'
+              position='absolute'
+              right='-5%'
+              w='25%'
+              top='-1em'
+            />
+          </Flex>
+        </Flex>
+        <Flex justifyContent='space-between' mt='1em'>
+          <Flex alignItems='center' mt='1em'>
+            <>
+              <Button
+                variant='mono-outline'
+                w='8em'
+                onClick={onEditingRowPerPageOpen}
+              >
+                {rowPerPage}
+              </Button>
+              <Modal
+                isOpen={isEditingRowPerPageOpen}
+                onClose={() => {
+                  onEditingRowPerPageClose();
+                }}
+              >
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Change Row Per Page</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
+                    <Input
+                      value={rowPerPage}
+                      onChange={(e) =>
+                        setRowPerPage(parseInt(e.target.value) | 0)
+                      }
+                    />
+                  </ModalBody>
+                </ModalContent>
+              </Modal>
+            </>
+            <Text ml='1em' fontWeight='bold' color='black'>
+              Records per page
+            </Text>
+          </Flex>
+          <Flex>
+            <Select
+              value={filterBy}
+              onChange={filterByChangeHandler}
+              placeholder='Filter By'
+              w='14em'
+            >
+              <option value='nim'>NIM</option>
+              <option value='name'>Name</option>
+            </Select>
+            <InputGroup ml='1em' w='20em'>
+              <Input
+                placeholder='Search'
+                value={searchQuery}
+                onChange={searchQueryChangeHandler}
+              />
+            </InputGroup>
+          </Flex>
+        </Flex>
+        {leaderboardQuery.isLoading ? (
+          <Text w='100%' fontStyle='italic' textAlign='center'>
+            Loading...
+          </Text>
+        ) : leaderboardQuery.isError ? (
+          <Text w='100%' color='salmon'>
+            Error
+          </Text>
+        ) : leaderboardList.length < 1 ? (
+          <Text w='100%'>Tidak ada data</Text>
+        ) : (
+          <Box
+            borderRadius='12px'
+            overflow='hidden'
+            mt='1em'
+            borderRight='1px solid'
+            borderLeft='1px solid'
+            borderColor='gray.400'
+          >
+            <Table w='100%' variant='black'>
+              <Thead>
+                <Td w='10%'>No.</Td>
+                <Td w='20%'>NIM</Td>
+                <Td w='20%'>Nama</Td>
+                <Td w='50%'>Poin</Td>
+              </Thead>
+              <Tbody>
+                {leaderboardList.map((data, index) => (
+                  <AddPointRow
+                    data={data}
+                    num={rowPerPage * (page - 1) + index + 1}
+                    key={index}
+                    editPoint={editPoint}
+                  />
+                ))}
+              </Tbody>
+            </Table>
+          </Box>
+        )}
+
+        <Flex
+          justifyContent='space-between'
+          mt='1em'
+          flexDir={{ base: 'column', lg: 'row-reverse' }}
+        >
+          <Flex justifyContent={{ base: 'space-between', lg: 'right' }}>
             <Button
               variant='mono-outline'
-              w='8em'
-              onClick={onEditingRowPerPageOpen}
-            >
-              {rowPerPage}
-            </Button>
-            <Modal
-              isOpen={isEditingRowPerPageOpen}
-              onClose={() => {
-                onEditingRowPerPageClose();
-              }}
-            >
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Change Row Per Page</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <Input
-                    value={rowPerPage}
-                    onChange={(e) =>
-                      setRowPerPage(parseInt(e.target.value) | 0)
-                    }
-                  />
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-          </>
-          <Text ml='1em' fontWeight='bold' color='black'>
-            Records per page
-          </Text>
-        </Flex>
-        <Flex>
-          <Select
-            value={filterBy}
-            onChange={filterByChangeHandler}
-            placeholder='Filter By'
-            w='14em'
-          >
-            <option value='nim'>NIM</option>
-            <option value='name'>Name</option>
-          </Select>
-          <InputGroup ml='1em' w='20em'>
-            <Input
-              placeholder='Search'
-              value={searchQuery}
-              onChange={searchQueryChangeHandler}
-            />
-          </InputGroup>
-        </Flex>
-      </Flex>
-      {leaderboardQuery.isLoading ? (
-        <Text w='100%' fontStyle='italic' textAlign='center'>
-          Loading...
-        </Text>
-      ) : leaderboardQuery.isError ? (
-        <Text w='100%' color='salmon'>
-          Error
-        </Text>
-      ) : leaderboardList.length < 1 ? (
-        <Text w='100%'>Tidak ada data</Text>
-      ) : (
-        <Box
-          borderRadius='12px'
-          overflow='hidden'
-          mt='1em'
-          borderRight='1px solid'
-          borderLeft='1px solid'
-          borderColor='gray.400'
-        >
-          <Table w='100%' variant='black'>
-            <Thead>
-              <Td w='10%'>No.</Td>
-              <Td w='20%'>NIM</Td>
-              <Td w='20%'>Nama</Td>
-              <Td w='50%'>Poin</Td>
-            </Thead>
-            <Tbody>
-              {leaderboardList.map((data, index) => (
-                <AddPointRow
-                  data={data}
-                  num={rowPerPage * (page - 1) + index + 1}
-                  key={index}
-                />
-              ))}
-            </Tbody>
-          </Table>
-        </Box>
-      )}
-
-      <Flex
-        justifyContent='space-between'
-        mt='1em'
-        flexDir={{ base: 'column', lg: 'row-reverse' }}
-      >
-        <Flex justifyContent={{ base: 'space-between', lg: 'right' }}>
-          <Button
-            variant='mono-outline'
-            w={{ base: '30%', lg: '6em' }}
-            mr='1em'
-            onClick={prevPage}
-          >
-            {'<'}
-          </Button>
-          <Menu>
-            <MenuButton
-              border='1px solid gray'
-              borderRadius='12px'
-              color='gray.600'
               w={{ base: '30%', lg: '6em' }}
-              h='2.5em'
+              mr='1em'
+              onClick={prevPage}
             >
-              {`${page}`}
-            </MenuButton>
-            <MenuList border='1px solid gray' p='1em'>
-              <Flex>
-                <Input value={jumpInput} onChange={jumpChangeHandler} />
-                <Button
-                  variant='mono-outline'
-                  w='8em'
-                  ml='1em'
-                  onClick={jumpToPage}
-                >
-                  Jump
-                </Button>
-              </Flex>
-            </MenuList>
-          </Menu>
-          <Button
-            variant='mono-outline'
-            w={{ base: '30%', lg: '6em' }}
-            ml='1em'
-            onClick={nextPage}
-          >
-            {'>'}
-          </Button>
+              {'<'}
+            </Button>
+            <Menu>
+              <MenuButton
+                border='1px solid gray'
+                borderRadius='12px'
+                color='gray.600'
+                w={{ base: '30%', lg: '6em' }}
+                h='2.5em'
+              >
+                {`${page}`}
+              </MenuButton>
+              <MenuList border='1px solid gray' p='1em'>
+                <Flex>
+                  <Input value={jumpInput} onChange={jumpChangeHandler} />
+                  <Button
+                    variant='mono-outline'
+                    w='8em'
+                    ml='1em'
+                    onClick={jumpToPage}
+                  >
+                    Jump
+                  </Button>
+                </Flex>
+              </MenuList>
+            </Menu>
+            <Button
+              variant='mono-outline'
+              w={{ base: '30%', lg: '6em' }}
+              ml='1em'
+              onClick={nextPage}
+            >
+              {'>'}
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </Layout>
   );
 };
--- src/pages/add-point.tsx
+++ src/pages/add-point.tsx
@@ -3,14 +3,15 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { Redirect } from '~/components/Redirect';
+import { AddPoint } from '~/components/add-point';
 import Layout from '~/layout';
 import { api } from '~/utils/api';
 
-export default function AddPoint() {
+export default function AddPointPage() {
   const { data: session } = useSession();
   const router = useRouter();
 
   if (!session) return <Redirect />;
-  if (session?.user.role === UserRole.MENTOR) return <AttendancePageMentor />;
+  if (session?.user.role === UserRole.MENTOR) return <AddPoint />;
   return <Redirect />;
 }
