99cc592a57a965d30762d7d96a40ea11ac251b6b
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-08-12 01:56:40 +0700

    fix: route protector

--- src/layout/AdminRoute.tsx
+++ src/layout/AdminRoute.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { type Session } from 'next-auth';
-import { Redirect } from '~/components/Redirect';
 import { UserRole } from '@prisma/client';
 
 interface Props {
@@ -11,22 +10,38 @@ interface Props {
 }
 
 const AdminRoute = ({ children, session, allowEO = false }: Props) => {
-  if (!session) {
-    return <Redirect />;
-  }
+  const router = useRouter();
 
-  if (allowEO) {
-    if (
-      session.user.role === UserRole.ADMIN ||
-      session.user.role === UserRole.EO
-    ) {
-      return <>{children}</>;
+  const [authorized, setAuthorized] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    authCheck();
+
+    // on route change start - hide page content by setting authorized to false
+    const hideContent = () => setAuthorized(false);
+    router.events.on('routeChangeStart', hideContent);
+
+    // on route change complete - run auth check
+    router.events.on('routeChangeComplete', authCheck);
+
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off('routeChangeStart', hideContent);
+      router.events.off('routeChangeComplete', authCheck);
+    };
+  }, []);
+
+  const authCheck = () => {
+    if (!session) {
+      void router.push('/');
+    } else if (session && session.user.role !== UserRole.ADMIN && !allowEO) {
+      void router.push('/group-management');
+    } else {
+      setAuthorized(true);
     }
-  } else {
-    if (session.user.role === UserRole.ADMIN) {
-      return <>{children}</>;
-    }
-  }
+  };
+
+  return authorized && <>{children}</>;
 };
 
 export default AdminRoute;
