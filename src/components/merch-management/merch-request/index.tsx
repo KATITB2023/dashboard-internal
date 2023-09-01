1a1756c756d531dc763063609e31b817fcbcfaa1
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-09-01 19:48:07 +0700

    feat: finish merch request

--- src/components/merch-management/merch-request/index.tsx
+++ src/components/merch-management/merch-request/index.tsx
@@ -15,9 +15,23 @@ import {
   useToast,
   Select
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { api } from '~/utils/api';
 import MerchRequestRow from './MerchRequestRow';
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
 
 export default function MerchRequest() {
   const toast = useToast();
@@ -27,10 +41,14 @@ export default function MerchRequest() {
   const [filterBy, setFilterBy] = useState('NIM');
   const [searchQuery, setSearchQuery] = useState<string>();
 
+  const searchValue = useDebounce(searchQuery);
+
+  console.log(searchValue, 'ini value');
+
   const requestQuery = api.merch.getMerchRequest.useQuery({
     page: page,
     filterBy: filterBy,
-    searchQuery: searchQuery
+    searchQuery: searchValue
   });
   const requestData = requestQuery.data?.data;
   const requestMetadata = requestQuery.data?.metadata;
@@ -89,9 +107,11 @@ export default function MerchRequest() {
     }
   };
 
-  return requestQuery.isLoading ? (
-    <Text>Loading</Text>
-  ) : requestMetadata && requestMetadata.total > 0 ? (
+  useEffect(() => {
+    setSearchQuery('');
+  }, [filterBy]);
+
+  return (
     <Flex direction={'column'} rowGap={4}>
       <Flex columnGap={4} w={'30%'} placeSelf={'flex-end'}>
         <Select onChange={(e) => setFilterBy(e.target.value)}>
@@ -106,11 +126,13 @@ export default function MerchRequest() {
         {filterBy === 'NIM' ? (
           <Input
             placeholder='Search NIM'
+            value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           />
         ) : (
           <Select
             placeholder='Select Merch'
+            defaultValue={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           >
             {merchNameList &&
@@ -124,102 +146,98 @@ export default function MerchRequest() {
           </Select>
         )}
       </Flex>
-      <TableContainer>
-        <Table variant={'black'}>
-          <Thead>
-            <Tr>
-              {header.map((item, index) => {
-                return (
-                  <Th key={index} w={item.w} textAlign={'center'}>
-                    {item.title}
-                  </Th>
-                );
-              })}
-            </Tr>
-          </Thead>
+      {requestQuery.isLoading ? (
+        <Text>Loading...</Text>
+      ) : requestMetadata && requestMetadata.total > 0 ? (
+        <>
+          <TableContainer>
+            <Table variant={'black'}>
+              <Thead>
+                <Tr>
+                  {header.map((item, index) => {
+                    return (
+                      <Th key={index} w={item.w} textAlign={'center'}>
+                        {item.title}
+                      </Th>
+                    );
+                  })}
+                </Tr>
+              </Thead>
 
-          <Tbody>
-            {requestData &&
-              requestData.map((item, index) => {
-                return (
-                  <MerchRequestRow
-                    data={item}
-                    index={(page - 1) * 5 + index + 1}
-                    key={item.id}
-                  />
-                );
-              })}
-          </Tbody>
-        </Table>
-      </TableContainer>
-      {!requestQuery.isLoading && (
-        <Flex
-          justifyContent={'space-between'}
-          flexDir={{ base: 'column-reverse', lg: 'row-reverse' }}
-          rowGap={'1rem'}
-        >
-          {requestMetadata && requestMetadata.total > 0 && (
-            <Flex>
-              <Button
-                variant='mono-outline'
-                w={{ base: '30%', lg: '4em' }}
-                mr='1em'
-                onClick={prevPage}
-              >
-                {'<'}
-              </Button>
-              <Menu>
-                <MenuButton
-                  border='1px solid gray'
-                  borderRadius='12px'
-                  color='gray.600'
+              <Tbody>
+                {requestData &&
+                  requestData.map((item, index) => {
+                    return (
+                      <MerchRequestRow
+                        data={item}
+                        index={(page - 1) * 5 + index + 1}
+                        key={item.id}
+                        emit={() => void requestQuery.refetch()}
+                      />
+                    );
+                  })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+          <Flex
+            justifyContent={'space-between'}
+            flexDir={{ base: 'column-reverse', lg: 'row-reverse' }}
+            rowGap={'1rem'}
+          >
+            {requestMetadata && requestMetadata.total > 0 && (
+              <Flex>
+                <Button
+                  variant='mono-outline'
                   w={{ base: '30%', lg: '4em' }}
-                  h='2.5em'
+                  mr='1em'
+                  onClick={prevPage}
                 >
-                  {page}
-                </MenuButton>
-                <MenuList border='1px solid gray' p='1em'>
-                  <Flex>
-                    <Input
-                      type='number'
-                      color={'white'}
-                      value={jumpInput}
-                      onChange={(e) => setJumpInput(e.target.value)}
-                    />
-                    <Button
-                      variant={'outline'}
-                      w='8em'
-                      ml='1em'
-                      onClick={jumpToPage}
-                    >
-                      Jump
-                    </Button>
-                  </Flex>
-                </MenuList>
-              </Menu>
-              <Button
-                variant='mono-outline'
-                w={{ base: '30%', lg: '4em' }}
-                ml='1em'
-                onClick={nextPage}
-              >
-                {'>'}
-              </Button>
-            </Flex>
-          )}
-
-          <Text>
-            Showing rows {requestMetadata.total === 0 ? 0 : (page - 1) * 5 + 1}{' '}
-            to{' '}
-            {page * 5 > requestMetadata.total
-              ? requestMetadata?.total
-              : page * 5}{' '}
-            of {requestMetadata?.total} entries
-          </Text>
-        </Flex>
+                  {'<'}
+                </Button>
+                <Menu>
+                  <MenuButton
+                    border='1px solid gray'
+                    borderRadius='12px'
+                    color='gray.600'
+                    w={{ base: '30%', lg: '4em' }}
+                    h='2.5em'
+                  >
+                    {page}
+                  </MenuButton>
+                  <MenuList border='1px solid gray' p='1em'>
+                    <Flex>
+                      <Input
+                        type='number'
+                        color={'white'}
+                        value={jumpInput}
+                        onChange={(e) => setJumpInput(e.target.value)}
+                      />
+                      <Button
+                        variant={'outline'}
+                        w='8em'
+                        ml='1em'
+                        onClick={jumpToPage}
+                      >
+                        Jump
+                      </Button>
+                    </Flex>
+                  </MenuList>
+                </Menu>
+                <Button
+                  variant='mono-outline'
+                  w={{ base: '30%', lg: '4em' }}
+                  ml='1em'
+                  onClick={nextPage}
+                >
+                  {'>'}
+                </Button>
+              </Flex>
+            )}
+          </Flex>
+        </>
+      ) : (
+        <Text>Tidak ada data</Text>
       )}
     </Flex>
-  ) : (
-    <Text>Tidak ada data</Text>
   );
 }
